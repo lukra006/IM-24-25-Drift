b2de874da41d8ecfab17d49f4bbf1d1ff67505cb
Joakim Tveit Husefest <johua023@Joakim-Tveit-Husefests-Mac.local>
2025-01-21 20:30:25 +0100

    Yepp

--- node-app/web-page/js/script.js
+++ node-app/web-page/js/script.js
@@ -5,8 +5,8 @@ const serverURL = 'http://localhost:3000';
     // Function to send a message
     async function sendMessage() {
 
-      let param1 = document.getElementById('ipadresse').value;
-      let param2 = document.getElementById('message').value;
+      let sender = document.getElementById('ipadresse').value;
+      let message = document.getElementById('message').value;
 
       if (!sender || !message) {
         alert('Please enter your name and message.');
@@ -17,7 +17,7 @@ const serverURL = 'http://localhost:3000';
         await fetch(`${serverURL}/send-message`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ param1, param2 }),
+          body: JSON.stringify({ sender: sender, message: message }),
         });
 
         document.getElementById('message').value = '';
@@ -29,17 +29,15 @@ const serverURL = 'http://localhost:3000';
 
     // Function to get all messages
     async function getMessages() {
-
-      let fra = document.getElementById('fra').innerHTML;
-      let melding = document.getElementById('melding').innerHTML;
       
       try {
         const response = await fetch('/get-messages');
 
         const messages = await response.json();
+        console.log(messages);
 
-        document.getElementById('fra').innerHTML = 'Fra: '+messages[0];
-        document.getElementById('melding').innerHTML = 'Melding: '+messages[1];
+        document.getElementById('fra').innerHTML = 'Fra: '+messages[0].sender;
+        document.getElementById('melding').innerHTML = 'Melding: '+messages[0].message;
 
       } catch (err) {
         console.error('Failed to fetch messages:', err);
